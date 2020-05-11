bfad806b7f9b0838d914d8457ccad9da48ed80c4
alexist98 <alexistoigo@gmail.com>
2020-05-11 11:12:19 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define IO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+#define FIN freopen("entrada.txt", "r", stdin);
+#define FOUT freopen("saida.sol", "w", stdout);
+
+#define MOD 1000000007
+#define ull unsigned long long int
+#define ll  long long int
+#define pii pair<int, int>
+#define pb push_back
+#define MAX 503
+
+const int INF = 0x3f3f3f3f;
+using namespace std;
+
+
+int main() {
+
+   IO
+
+   return 0;
+}
