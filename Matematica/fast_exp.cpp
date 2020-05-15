73ac62a26cab33de2299cad92369990e054dabdf
alexist98 <alexistoigo@gmail.com>
2020-05-15 11:08:32 -0300

    Update fast_exp.cpp

--- Matematica/fast_exp.cpp
+++ Matematica/fast_exp.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 #define IO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
-#define FIN freopen("entrada.txt", "r", stdin);
-#define FOUT freopen("saida.sol", "w", stdout);
 
 #define MOD 1000000007
 #define ull unsigned long long int
 #define ll  long long int
-#define pii pair<int, int>
-#define pb push_back
-#define MAX int(5*10e5+2)
 
-const int INF = 0x3f3f3f3f;
 using namespace std;
 
+/*
+EXPONENCIAÇÃO RÁPIDA:
+OPERAÇÕES O(log(n))
+MEMORIA O(1)
+*/
+
 ll fastExp(ll base, ll e) {
-   if(e==0)
-      return 1;
-   ll resp=fastExp(base, e/2);
-   resp*=resp;
-   if(e%2==1)
-      resp*=base;
+   ll resp=1;
+
+   while(e>0) {
+      if(e%2==1)
+         resp*=base;
+      base*=base;
+      e/=2;
+   }
 
    return resp;
 }
-
 
 int main() {
 
